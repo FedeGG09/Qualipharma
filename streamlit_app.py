2ec5c0f61fdc5a051e1b400648ca8cb8576fc7a3
Federico Guillermo Gravina <gravinadavilafederico@gmail.com>
2024-05-20 10:04:12 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,16 +42,16 @@ def generate_response(prompt_input, email, passwd):
     return chatbot.chat(prompt)
 
 # User-provided prompt
-if prompt := st.chat_input(disabled=not (hf_email and hf_pass)):
+if st.chat_input():
+    prompt = st.session_state.messages
     st.session_state.messages.append({"role": "user", "content": prompt})
-    with st.chat_message("user"):
-        st.write(prompt)
+    st.write(prompt)
 
 # Generate a new response if last message is not from assistant
 if st.session_state.messages[-1]["role"] != "assistant":
     with st.chat_message("assistant"):
         with st.spinner("Thinking..."):
-            response = generate_response(prompt, hf_email, hf_pass) 
+            response = generate_response(prompt) 
             st.write(response) 
     message = {"role": "assistant", "content": response}
     st.session_state.messages.append(message)
